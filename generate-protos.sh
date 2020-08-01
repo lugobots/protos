d2ce12f6770da1d896626887b9616767eb55736a
Rubens Silva <rubens21@gmail.com>
2020-08-01 12:29:30 -0400

    experiments and adjusts

--- generate-protos.sh
+++ generate-protos.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 SRC_DIR=$(pwd)/proto
+DST_DIR=$(pwd)/lugo
+
 echo "${SRC_DIR}"
 
 cd proto
 mkdir -p lugo
 protoc -I=/usr/local/include/ -I=$SRC_DIR --go_out=plugins=grpc:lugo/  *.proto
 
-mv lugo/*.pb.go ../lugo/
-rmdir lugo
+#python -m pip install grpcio
+#python -m pip install grpcio-tools
+python -m grpc_tools.protoc  -I=$SRC_DIR --python_out=${DST_DIR} --grpc_python_out=${SRC_DIR} *.proto
