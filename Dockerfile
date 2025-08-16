cd41b02355c781989ff3a6183e93a4ff47668435
Rubens Silva <rubens21@gmail.com>
2025-08-15 20:42:59 -0600

    checkpoint Golden goal

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ ENV PROTOC_VERSION=27.1
 
 # Install system dependencies
 RUN apt-get update && apt-get install -y \
-    curl unzip git build-essential python3 python3-pip npm node-gyp \
+    curl unzip git build-essential python3 python3-pip npm node-gyp autoconf libtool protobuf-compiler \
     && rm -rf /var/lib/apt/lists/*
 
 # Install Go
@@ -27,7 +27,11 @@ RUN go install google.golang.org/protobuf/cmd/protoc-gen-go@latest \
 RUN pip3 install --break-system-packages grpcio grpcio-tools
 
 # Install Node.js gRPC tools and dependencies
-RUN npm install -g node-pre-gyp grpc-tools protoc-gen-grpc
+RUN npm install -g node-pre-gyp grpc-tools protoc-gen-grpc grpc_tools_node_protoc_ts@5.3.2 protoc-gen-ts
+RUN npm i -g grpc-tools@1.11.2 --unsafe-perm
+RUN npm i -g typescript --unsafe-perm
+RUN ln -s /out/node_modules/.bin/protoc-gen-ts /bin/grpc_tools_node_protoc
+
 
 # Working directory for mounted volumes
 WORKDIR /work
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ set -e
 SRC_DIR=/source
 OUT_DIR=/output
 
-rm -rf $OUT_DIR/* && echo "" || exit 1
+rm -rf $OUT_DIR/* && echo "Deleting previous files" || (echo "Failed to delete previous file" && exit 1)
 
 mkdir -p $OUT_DIR/go $OUT_DIR/python $OUT_DIR/nodejs
 
@@ -15,20 +15,27 @@ mkdir -p $OUT_DIR/go $OUT_DIR/python $OUT_DIR/nodejs
 protoc -I $SRC_DIR \
     --go_out=paths=source_relative:$OUT_DIR/go \
     --go-grpc_out=paths=source_relative:$OUT_DIR/go \
-    $(find $SRC_DIR -name "*.proto")
+    $(find $SRC_DIR -name "*.proto") && echo "Go code generated" || (echo "Failed to generate Go code" && exit 1)
 
 # Generate Python code
 python3 -m grpc_tools.protoc -I $SRC_DIR \
     --python_out=$OUT_DIR/python \
     --grpc_python_out=$OUT_DIR/python \
-    $(find $SRC_DIR -name "*.proto")
+    $(find $SRC_DIR -name "*.proto") && echo "Python code generated" || (echo "Failed to generate Python code" && exit 1)
 
-## Generate Node.js code
-#protoc -I $SRC_DIR \
-#  --js_out=import_style=commonjs,binary:$OUT_DIR/nodejs \
-#  --grpc_out=$OUT_DIR/nodejs \
-#  --plugin=protoc-gen-grpc=$(which grpc_tools_node_protoc_plugin) \
-#  $(find $SRC_DIR -name "*.proto")
+# Generate Node.js code
+grpc_tools_node_protoc \
+      --grpc_out=grpc_js:$OUT_DIR/nodejs \
+      --js_out=import_style=commonjs,binary:$OUT_DIR/nodejs  \
+      -I $SRC_DIR $(find $SRC_DIR -name "*.proto") && echo "JS code generated" || (echo "Failed to generate JS code" && exit 1)
+
+grpc_tools_node_protoc  --help
+
+# Generating Typescript typing
+grpc_tools_node_protoc \
+      --plugin=protoc-gen-ts=/bin/grpc_tools_node_protoc \
+      --ts_out=grpc_js:$OUT_DIR/nodejs \
+      -I $SRC_DIR $(find $SRC_DIR -name "*.proto") && echo "TS code generated" || (echo "Failed to generate TS code" && exit 1)
 
 
 echo "✅ Protobuf generation complete!"
